d6bd852995f1942aba3c7a515e3642f690b129b3
imcalledpedro <imcalledpedro@gmail.com>
2019-11-15 07:49:19 -0300

    p

--- impact.sql
+++ impact.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 13-Nov-2019 às 17:55
+-- Generation Time: 15-Nov-2019 às 11:48
 -- Versão do servidor: 10.1.38-MariaDB
 -- versão do PHP: 7.3.2
 
@@ -39,15 +39,17 @@ CREATE TABLE `produtos` (
   `link_1` varchar(2000) COLLATE utf8_bin NOT NULL,
   `link_2` varchar(2000) COLLATE utf8_bin DEFAULT NULL,
   `link_3` varchar(2000) COLLATE utf8_bin DEFAULT NULL,
-  `link_4` varchar(2000) COLLATE utf8_bin DEFAULT NULL
+  `link_4` varchar(2000) COLLATE utf8_bin DEFAULT NULL,
+  `link_imagem` varchar(800) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id`, `nome`, `descricao`, `categoria`, `link_1`, `link_2`, `link_3`, `link_4`) VALUES
-(9, 'Escova de dentes de bambu', 'teste', 'teste', 'link', 'link', 'link', 'link');
+INSERT INTO `produtos` (`id`, `nome`, `descricao`, `categoria`, `link_1`, `link_2`, `link_3`, `link_4`, `link_imagem`) VALUES
+(1, 'Escova de dentes de bambu', 'Feita de bambu e cerdas de carvão', 'Higiene', 'link', 'link', 'link', 'link', 'https://lojapensandoaocontrario.com.br/wp-content/uploads/2018/01/escova_ecologica_bambu_embalagem_eco2.jpg'),
+(2, 'Copo de papel biodegradável', '240ml', '', '', NULL, NULL, NULL, 'https://img.elo7.com.br/product/zoom/295713D/150-copos-de-papel-branco-biodegradavel-240ml-hefei-copos-biodegradaveis.jpg');
 
 -- --------------------------------------------------------
 
@@ -70,8 +72,8 @@ CREATE TABLE `publicacoes` (
 --
 
 INSERT INTO `publicacoes` (`id`, `titulo`, `descricao`, `texto_completo`, `data_publicacao`, `anexo`) VALUES
-(4, 'Canudos descartáveis e as soluções possíveis', 'Empresários se juntam à luta contra os plásticos descartáveis e lançam modelos de canudo biodegradável, compostável e até comestível', 'Os canudos de plástico se tornaram um grande problema ecológico. Eles já representam 4% de todo o lixo plástico produzido no mundo e, como são feitos dos plásticos polipropileno e poliestireno, não são biodegradáveis e sua reciclagem é difícil. Eles podem levar até mil anos para se decompor no meio ambiente. Mas será que são mesmo necessários? O mercado tem se ajustado à luta contra os descartáveis e começam a surgir alternativas. O canudo de papel é cada vez mais comum e já existem também modelos de canudinho biodegradável e até comestível.\r\n\r\nO canudo de plástico é um item a ser evitado. Mesmo quando descartado corretamente, ele pode escapar para a natureza e ser carregado pela chuva para mares e rios, impactando toda a fauna aquática. Estima-se que 90% das espécies marinhas tenham ingerido produtos de plástico em algum momento. Além disso, nas praias e mares, esses canudos são também fonte de formação de microplástico, o pior formato possível para o plástico e que já está presente nos alimentos, no sal, na água potável e até em garrafas de água mineral! Saiba mais sobre os impactos e alternativas ao consumo do canudo de plástico.\r\n\r\nPreocupados com o meio ambiente e com os impactos do lixo plástico, alguns empresários já lançaram alternativas ao canudo de plástico. O canudo de papel é cada vez mais presente, já que é uma opção biodegradável, mas ele também acaba sendo fonte de poluição até sua completa biodegradação, já que o modelo foi pensado como um descartável. Opções melhores são os canudos comestíveis e os modelos de canudo compostável.', '2019-09-02 22:56:00', ''),
-(5, 'lorem', 'lorem', 'Os canudos de plástico se tornaram um grande problema ecológico. Eles já representam 4% de todo o lixo plástico produzido no mundo e, como são feitos dos plásticos polipropileno e poliestireno, não são biodegradáveis e sua reciclagem é difícil. Eles podem levar até mil anos para se decompor no meio ambiente. Mas será que são mesmo necessários? O mercado tem se ajustado à luta contra os descartáveis e começam a surgir alternativas. O canudo de papel é cada vez mais comum e já existem também modelos de canudinho biodegradável e até comestível.\r\n\r\nO canudo de plástico é um item a ser evitado. Mesmo quando descartado corretamente, ele pode escapar para a natureza e ser carregado pela chuva para mares e rios, impactando toda a fauna aquática. Estima-se que 90% das espécies marinhas tenham ingerido produtos de plástico em algum momento. Além disso, nas praias e mares, esses canudos são também fonte de formação de microplástico, o pior formato possível para o plástico e que já está presente nos alimentos, no sal, na água potável e até em garrafas de água mineral! Saiba mais sobre os impactos e alternativas ao consumo do canudo de plástico.\r\n\r\nPreocupados com o meio ambiente e com os impactos do lixo plástico, alguns empresários já lançaram alternativas ao canudo de plástico. O canudo de papel é cada vez mais presente, já que é uma opção biodegradável, mas ele também acaba sendo fonte de poluição até sua completa biodegradação, já que o modelo foi pensado como um descartável. Opções melhores são os canudos comestíveis e os modelos de canudo compostável.', '2019-11-12 00:00:00', '');
+(4, 'Canudos descartáveis e as soluções possíveis', 'Empresários se juntam à luta contra os plásticos descartáveis e lançam modelos de canudo biodegradável, compostável e até comestível', 'Os canudos de plástico se tornaram um grande problema ecológico. Eles já representam 4% de todo o lixo plástico produzido no mundo e, como são feitos dos plásticos polipropileno e poliestireno, não são biodegradáveis e sua reciclagem é difícil. Eles podem levar até mil anos para se decompor no meio ambiente. Mas será que são mesmo necessários? O mercado tem se ajustado à luta contra os descartáveis e começam a surgir alternativas. O canudo de papel é cada vez mais comum e já existem também modelos de canudinho biodegradável e até comestível.\r\n<br><br>\r\nO canudo de plástico é um item a ser evitado. Mesmo quando descartado corretamente, ele pode escapar para a natureza e ser carregado pela chuva para mares e rios, impactando toda a fauna aquática. Estima-se que 90% das espécies marinhas tenham ingerido produtos de plástico em algum momento. Além disso, nas praias e mares, esses canudos são também fonte de formação de microplástico, o pior formato possível para o plástico e que já está presente nos alimentos, no sal, na água potável e até em garrafas de água mineral! Saiba mais sobre os impactos e alternativas ao consumo do canudo de plástico.\r\n<br><br>\r\nPreocupados com o meio ambiente e com os impactos do lixo plástico, alguns empresários já lançaram alternativas ao canudo de plástico. O canudo de papel é cada vez mais presente, já que é uma opção biodegradável, mas ele também acaba sendo fonte de poluição até sua completa biodegradação, já que o modelo foi pensado como um descartável. Opções melhores são os canudos comestíveis e os modelos de canudo compostável.', '2019-09-02 22:56:00', ''),
+(5, 'lorem', 'lorem', 'Os canudos de plástico se tornaram um grande problema ecológico. Eles já representam 4% de todo o lixo plástico produzido no mundo e, como são feitos dos plásticos polipropileno e poliestireno, não são biodegradáveis e sua reciclagem é difícil. Eles podem levar até mil anos para se decompor no meio ambiente. Mas será que são mesmo necessários? O mercado tem se ajustado à luta contra os descartáveis e começam a surgir alternativas. O canudo de papel é cada vez mais comum e já existem também modelos de canudinho biodegradável e até comestível.\r\n<br><br>\r\nO canudo de plástico é um item a ser evitado. Mesmo quando descartado corretamente, ele pode escapar para a natureza e ser carregado pela chuva para mares e rios, impactando toda a fauna aquática. Estima-se que 90% das espécies marinhas tenham ingerido produtos de plástico em algum momento. Além disso, nas praias e mares, esses canudos são também fonte de formação de microplástico, o pior formato possível para o plástico e que já está presente nos alimentos, no sal, na água potável e até em garrafas de água mineral! Saiba mais sobre os impactos e alternativas ao consumo do canudo de plástico.\r\n<br><br>\r\nPreocupados com o meio ambiente e com os impactos do lixo plástico, alguns empresários já lançaram alternativas ao canudo de plástico. O canudo de papel é cada vez mais presente, já que é uma opção biodegradável, mas ele também acaba sendo fonte de poluição até sua completa biodegradação, já que o modelo foi pensado como um descartável. Opções melhores são os canudos comestíveis e os modelos de canudo compostável.', '2019-11-12 00:00:00', '');
 
 -- --------------------------------------------------------
 
@@ -127,7 +129,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `publicacoes`
